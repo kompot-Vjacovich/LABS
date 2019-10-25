aebcf97328f609fd00e271394dd4a73df86017f0
kompot-Vjacovich <55319745+kompot-Vjacovich@users.noreply.github.com>
2019-10-25 23:46:44 +0300

    without scale

--- Bayes/Levels.R
+++ Bayes/Levels.R
@@ -1,17 +1,30 @@
-Ng <- function(x, mu, mtx) { #n-мерное гауссовское распределение
-  a11 <- mtx[1,1]
+draw_lines <- function(mu, mtx, title) { #n-мерное гауссовское распределение
+  a11 <- mtx[2,2]
   a12 <- mtx[1,2]
   a21 <- mtx[2,1]
-  a22 <- mtx[2,2]
+  a22 <- mtx[1,1]
   
   det = det(mtx)
   
   A <- a22/det
-  B <- -(a12+a21)/det
-  C <- a11/det
-  D <- 
+  B <- a11/det
+  C <- (-a12-a21)/det
+  D <- (-2*a22*mu[1] + mu[2]*(a12+a21))/det
+  E <- (-2*a11*mu[2] + mu[1]*(a12+a21))/det
+  F <- (a22*mu[1]^2 + a11*mu[2]^2 - mu[1]*mu[2]*(a12+a21))/det
   
-  (1/(sqrt((2*pi)^2*det(mtx))))*exp((-1/2)*...)
+  N <- function(x,y) (1/(sqrt((2*pi)^2*det(mtx))))*exp((-1/2)*(x^2*A + y^2*B + x*y*C + x*D + y*E + F))
+  
+  x <- seq(-4, 4, 8/200)
+  y <- seq(-4, 4, 8/200)
+  z <- outer(x, y, N)
+  
+  contour(x, y, z, main=title)
+  
 }  
 
-Ng(c(1, 1), matrix(0,1,2), matrix(c(1,0,0,1),2,2))
+
+draw_lines(c(0,0), matrix(c(1,0,0,1), 2, 2), title="Признаки имеют одинаковые дисперсии")
+#draw_lines(c(0,0), matrix(c(1,1,0,1), nrow=2, ncol=2), title="Признаки коррелированы")
+#draw_lines(c(0,0), matrix(c(3,0,0,1), nrow=2, ncol=2), title="Признаки некоррелированы")
+#draw_lines(c(0,0), matrix(c(1,0,0,3), nrow=2, ncol=2), title="Признаки некоррелированы")
